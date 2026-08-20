3e8f75c286ea5730d5ceda9a913e2075a1b4b1bf
integrationuser-pixel <integration.user@sarlogisolutions.com>
2026-08-20 17:16:44 +0530

    fix: add mapping_sales_targets and mapping_zone_targets to WIPEABLE_COLLS

--- api/mongo-batch.js
+++ api/mongo-batch.js
@@ -2824,6 +2824,7 @@ module.exports = async function handler(req, res) {
         "jobs_isotank_export","jobs_isotank_import","jobs_general","jobs_road",
         "jobs_clearance_export","jobs_clearance_import",
         "srr_air_export","srr_air_import","srr_sea_export","srr_sea_import",
+        "mapping_sales_targets","mapping_zone_targets"
       ];
       if (!WIPEABLE_COLLS.includes(collName)) return res.status(400).json({ error: "unknown collection" });
       const result = await db.collection(collName).deleteMany({});
